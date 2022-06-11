fadd90f8f2100f35509d79b755f8c794ffd367c4
Young <juneyoung-jo@github.com>
2022-06-12 03:51:15 +0900

    [SHOP-10]-chore : remove unused import

--- merchandise-api/src/main/kotlin/com/hindsight/merchandies_api/MerchandiseApplication.kt
+++ merchandise-api/src/main/kotlin/com/hindsight/merchandies_api/MerchandiseApplication.kt
@@ -1,6 +1,5 @@
 package com.hindsight.merchandies_api
 
-import com.hindsight.core.EnableCore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
