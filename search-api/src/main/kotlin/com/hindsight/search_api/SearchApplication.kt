3101daf7a54ae471594acef0e2b13de43a152915
Young <juneyoung-jo@github.com>
2022-06-13 23:44:34 +0900

    [SHOP-10]-feat : Search module Jpa & QueryDsl Setting 추가

--- search-api/src/main/kotlin/com/hindsight/search_api/SearchApplication.kt
+++ search-api/src/main/kotlin/com/hindsight/search_api/SearchApplication.kt
@@ -1,11 +1,9 @@
 package com.hindsight.search_api
 
-import com.hindsight.core.EnableCore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableCore
 class SearchApplication
 
 fun main(args: Array<String>) {
---
+++ search-api/src/main/kotlin/com/hindsight/search_api/domain/ItemEntity.kt
@@ -0,0 +1,32 @@
+package com.hindsight.search_api.domain
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "item")
+class ItemEntity private constructor(
+    title: String,
+    text: String
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    var id: Long? = null
+
+
+    var title: String = title
+    var text: String = text
+    var deleted: Boolean = false
+
+    companion object {
+        fun of(
+            title: String,
+            text: String
+        ): ItemEntity = ItemEntity(
+            title = title,
+            text = text
+        )
+    }
+
+}
