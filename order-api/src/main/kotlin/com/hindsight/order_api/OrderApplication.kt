5fade05b8e8a3f58d8118399f0cb7232224fe9db
Young <juneyoung-jo@github.com>
2022-06-13 23:29:55 +0900

    [SHOP-10]-feat : Order module Jpa & QueryDsl Setting 추가

--- order-api/src/main/kotlin/com/hindsight/order_api/OrderApplication.kt
+++ order-api/src/main/kotlin/com/hindsight/order_api/OrderApplication.kt
@@ -1,11 +1,9 @@
 package com.hindsight.order_api
 
-import com.hindsight.core.EnableCore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableCore
 class OrderApplication
 
 fun main(args: Array<String>) {
