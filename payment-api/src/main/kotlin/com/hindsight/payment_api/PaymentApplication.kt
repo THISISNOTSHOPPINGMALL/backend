a467661178022cddf307bdf30d139b901f745777
Young <juneyoung-jo@github.com>
2022-06-13 23:40:13 +0900

    [SHOP-10]-feat : Payment module Jpa & QueryDsl Setting 추가

---
+++ payment-api/src/main/kotlin/com/hindsight/payment_api/PaymentApplication.kt
@@ -0,0 +1,11 @@
+package com.hindsight.payment_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentApplication>(*args)
+}
